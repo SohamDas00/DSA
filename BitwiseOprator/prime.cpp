cfed63f14f9f990aa437f18ee3bd58016d2e198b
SohamDas00 <jisusoham04@gmail.com>
2025-06-26 23:56:43 +0530

    complete prime Q

---
+++ BitwiseOprator/prime.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int num;
+    cout<<"Enter number: ";
+    cin>>num;
+    for(int i=2;i<num;i++){
+        if(num%i==0){
+            cout<<num<<" is not Prime";
+            break;
+        }
+        else{
+            cout<<num<<" is Prime";
+            break;
+        }
+    }
+}
