338d07298b42b265942e1eb3d86c081be5f7a128
SohamDas00 <jisusoham04@gmail.com>
2025-06-26 23:57:12 +0530

    complete continue Q

---
+++ BitwiseOprator/continue.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    for(int i=0;i<5;i++){
+        cout<<"Hi"<<endl;
+        cout<<"Hello"<<endl;
+        continue;
+        cout<<"Message will not print"<<endl;
+    }
+    cout<<"THE END";
+}
