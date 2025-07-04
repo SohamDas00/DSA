73aa50928ba2baf3e556ebbdf583fcb8ec233362
SohamDas00 <jisusoham04@gmail.com>
2025-07-04 23:49:10 +0530

    complete  basic array

---
+++ Array/basic.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int arr1[10]={1,2,4,6,7};
+    cout<<"The array1 is: ";
+    for(int i=0;i<10;i++){
+        cout<<arr1[i]<<",";
+    }
+    cout<<endl;
+    char arr2[10]={'a','f','t','p','r'};
+    cout<<"The array2 is: ";
+    for(int i=0;i<10;i++){
+        cout<<arr2[i]<<",";
+    }
+}
