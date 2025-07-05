5f8ac11dcea109cc0f9095460662c6b599fede00
SohamDas00 <jisusoham04@gmail.com>
2025-07-05 12:59:13 +0530

    modify linear search code

--- Array/linearsearch.cpp
+++ Array/linearsearch.cpp
@@ -2,5 +2,26 @@
 using namespace std;
 
 int main(){
-    int 
+    int arr[100];
+    int size,target,flag=0,index;
+    cout<<"Enter target: ";
+    cin>>target;
+    cout<<"Enter size: ";
+    cin>>size;
+    cout<<"The array is: ";
+    for(int i=0;i<size;i++){
+        cin>>arr[i];
+    }
+    for(int i=0;i<size;i++){
+        if(target==arr[i]){
+            flag=1;
+            index=i;
+        }
+    }
+    if(flag==1){
+        cout<<target<<" found at index "<<index;
+    }
+    else{
+        cout<<target<<" not found";
+    }
 }
